01ac9e194fcb0bb55d68e73798ec78fafabe5f49
Kirill Shishkin <kirillshishkin77@gmail.com>
2020-08-02 02:00:05 +0300

    CROS support provided

--- src/test/java/com/kirill/restAPI/DatabaseOperationsTest.java
+++ src/test/java/com/kirill/restAPI/DatabaseOperationsTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,6 +17,7 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
